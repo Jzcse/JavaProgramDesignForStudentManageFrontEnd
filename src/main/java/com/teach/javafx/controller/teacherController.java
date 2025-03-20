074cd123d5b16cb04c5f221473079d06c333bda3
Jzcse <3294582930@qq.com>
2025-03-20 15:09:11 +0800

    1. 为教师管理系统增加了分页搞作。 2. 优化了教师管理系统的部分BUG。 3. 优化了教师管理系统的操作体验。 4. 移除了Herobrine。

--- src/main/java/com/teach/javafx/controller/teacherController.java
+++ src/main/java/com/teach/javafx/controller/teacherController.java
@@ -8,6 +8,7 @@ import com.teach.javafx.request.DataResponse;
 import com.teach.javafx.request.HttpRequestUtil;
 import com.teach.javafx.request.OptionItem;
 import com.teach.javafx.util.CommonMethod;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -48,7 +49,7 @@ public class teacherController extends ToolController {
     private  TableColumn<Map<String, String>, String> phoneColumn;  //教师电话
     @FXML
     private  TableColumn<Map<String, String>, String> addressColumn;//教师地址
-
+    //components of right box for detail info of teacher
     @FXML
     private TextField numField;
     @FXML
@@ -73,14 +74,31 @@ public class teacherController extends ToolController {
     private DatePicker birthdayPick;
     @FXML
     private TextField numNameTextField;
+    //pagination component
+    @FXML
+    private Pagination pagination;
 
 
     //service
     private boolean isNew = false;
+    private int teacherCount;
+    private int pageCount;
     private Integer personId;
     private ArrayList<Map<String, String>> teacherList = new ArrayList<>();
     private ObservableList<Map<String, String>> observableList = FXCollections.observableArrayList();
     private List<OptionItem> genderList;
+
+    public void refreshTeacherList(){
+        int currentPage = (int) pagination.getCurrentPageIndex();
+        DataRequest dataRequest = new DataRequest();
+        dataRequest.add("page", currentPage);
+        dataRequest.add("size", 35);
+        DataResponse dataResponse = HttpRequestUtil.request("/api/teacher/getTeacherList", dataRequest);
+        if (dataResponse != null && dataResponse.getCode() == 0){
+            teacherList = (ArrayList<Map<String, String>>) dataResponse.getData();
+            setTableViewData();
+        }
+    }
 
     public void setTableViewData(){
         observableList.clear();
@@ -90,11 +108,28 @@ public class teacherController extends ToolController {
         dataTableView.setItems(observableList);
     }
 
+    public void refreshPageCount(){
+        DataRequest dataRequestForTeacherCount = new DataRequest();
+        DataResponse dataResponse = HttpRequestUtil.request("/api/teacher/getTeacherCount", dataRequestForTeacherCount);
+        if (dataResponse != null && dataResponse.getCode() == 0){
+            Map<String, String> map = (Map<String, String>) dataResponse.getData();
+            teacherCount = Integer.parseInt(CommonMethod.getString(map, "count"));
+            if (teacherCount % 35 == 0){
+                pageCount = teacherCount / 35;
+            } else {
+                pageCount = teacherCount / 35 + 1;
+            }
+            pagination.setPageCount(pageCount);
+        } else {
+            MessageDialog.showDialog("Error! Failed to get teacher count from the server.");
+        }
+    }
+
     @FXML
     public void initialize() {
         DataRequest dataRequest = new DataRequest();
         dataRequest.add("page", 0);
-        dataRequest.add("size", 50);
+        dataRequest.add("size", 35);
         DataResponse dataResponse = HttpRequestUtil.request("/api/teacher/getTeacherList", dataRequest);
         if (dataResponse != null && dataResponse.getCode() == 0){
             teacherList = (ArrayList<Map<String, String>>) dataResponse.getData();
@@ -105,11 +140,31 @@ public class teacherController extends ToolController {
         majorColumn.setCellValueFactory(new MapValueFactory("major"));
         titleColumn.setCellValueFactory(new MapValueFactory("title"));
         cardColumn.setCellValueFactory(new MapValueFactory("card"));
-        genderColumn.setCellValueFactory(new MapValueFactory("genderName"));
+        genderColumn.setCellValueFactory(new MapValueFactory("gender"));
+        genderColumn.setCellValueFactory(cellData -> {
+            int genderValue = Integer.parseInt(cellData.getValue().get("gender"));
+            String genderName;
+            if (genderValue == 1) {
+                genderName = "男";
+            } else if (genderValue == 2) {
+                genderName = "女";
+            } else {
+                genderName = "";
+            }
+            return new SimpleStringProperty(genderName);
+        });
         birthdayColumn.setCellValueFactory(new MapValueFactory("birthday"));
         emailColumn.setCellValueFactory(new MapValueFactory("email"));
         phoneColumn.setCellValueFactory(new MapValueFactory("phone"));
         addressColumn.setCellValueFactory(new MapValueFactory("address"));
+        //get the sum count of teacher and init the page count
+        refreshPageCount();
+        //init the pagination
+        pagination.setCurrentPageIndex(0);
+        pagination.currentPageIndexProperty().addListener((observable, oldValue, newValue) -> {
+            //refresh teacher list and set table view
+            refreshTeacherList();
+        });
         //copy teacher's code
         //选中状态处理
         TableView.TableViewSelectionModel<Map<String, String>> tableViewSelectionModel = dataTableView.getSelectionModel();
@@ -119,6 +174,7 @@ public class teacherController extends ToolController {
         //处理性别下拉框
         genderList = HttpRequestUtil.getDictionaryOptionItemList("XBM");
         genderComboBox.getItems().addAll(genderList);
+        //设置日期格式
         birthdayPick.setConverter(new LocalDateStringConverter("yyyy-MM-dd"));
     }
 
@@ -191,15 +247,10 @@ public class teacherController extends ToolController {
                 if (dataResponse != null){
                     if (dataResponse.getCode() == 0){
                         MessageDialog.showDialog("删除成功！");
+                        //刷新教师数量及页码数
+                        refreshPageCount();
                         //刷新表格
-                        DataRequest dataRequest1 = new DataRequest();
-                        dataRequest1.add("page", 0);
-                        dataRequest1.add("size", 50);
-                        DataResponse dataResponse1 = HttpRequestUtil.request("/api/teacher/getTeacherList", dataRequest1);
-                        if (dataResponse1 != null && dataResponse1.getCode() == 0){
-                            teacherList = (ArrayList<Map<String, String>>) dataResponse1.getData();
-                            setTableViewData();
-                        }
+                        refreshTeacherList();
                     } else {
                         MessageDialog.showDialog(dataResponse.getMsg());
                     }
@@ -213,9 +264,9 @@ public class teacherController extends ToolController {
 
     public void onQueryButtonClick(ActionEvent actionEvent) {
         String input = numNameTextField.getText();
+        DataRequest dataRequest = new DataRequest();
         if (input.matches(".*\\d.*")){
             //num query
-            DataRequest dataRequest = new DataRequest();
             dataRequest.add("num", input);
             DataResponse dataResponse = HttpRequestUtil.request("/api/teacher/searchTeacherByNum", dataRequest);
             if (dataResponse != null) {
@@ -229,7 +280,6 @@ public class teacherController extends ToolController {
 
         } else {
             //name query
-            DataRequest dataRequest = new DataRequest();
             dataRequest.add("name", input);
             DataResponse dataResponse = HttpRequestUtil.request("/api/teacher/searchTeacherByName", dataRequest);
 
@@ -268,7 +318,13 @@ public class teacherController extends ToolController {
             personForm.put("card", cardField.getText());
             if (genderComboBox.getSelectionModel() != null && genderComboBox.getSelectionModel().getSelectedItem() != null)
                 personForm.put("gender", genderComboBox.getSelectionModel().getSelectedItem().getValue());
-            personForm.put("birthday", birthdayPick.getEditor().getText());
+            String birthdayTemp = birthdayPick.getEditor().getText();
+            if (!birthdayTemp.matches("^(?:(?:19|20)\\d{2})-(?:(?:0[1-9]|1[0-2]))-(?:(?:0[1-9]|[12]\\d|3[01]))$")){
+                MessageDialog.showDialog("日期不符合规范,请检查。");
+                return;
+            } else {
+                personForm.put("birthday", birthdayPick.getEditor().getText());
+            }
             personForm.put("email", emailField.getText());
             personForm.put("phone", phoneField.getText());
             personForm.put("address", addressField.getText());
@@ -278,21 +334,19 @@ public class teacherController extends ToolController {
             DataResponse dataResponse = HttpRequestUtil.request("/api/teacher/addTeacher", dataRequest);
             if (dataResponse.getCode() == 0){
                 MessageDialog.showDialog("添加教师成功");
-                //刷新表格
-                DataRequest dataRequest1 = new DataRequest();
-                dataRequest1.add("page", 0);
-                dataRequest1.add("size", 50);
-                DataResponse dataResponse1 = HttpRequestUtil.request("/api/teacher/getTeacherList", dataRequest1);
-                if (dataResponse1 != null && dataResponse1.getCode() == 0){
-                    teacherList = (ArrayList<Map<String, String>>) dataResponse1.getData();
-                    setTableViewData();
-                }
+                //刷新教师数与页数
+                refreshPageCount();
+                //设置页码值
+                pagination.setCurrentPageIndex(pageCount - 1);
+                //刷新教师列表并刷新界面
+                refreshTeacherList();
+                //clear
+                clearPanel();
             } else {
                 MessageDialog.showDialog(dataResponse.getMsg());
             }
-
         } else {
-            if (numField.getText().equals("")) {
+            if (numField.getText().isEmpty()) {
                 MessageDialog.showDialog("修改后工号不能为空");
                 return;
             }
@@ -309,7 +363,13 @@ public class teacherController extends ToolController {
             form.put("card", cardField.getText());
             if (genderComboBox.getSelectionModel() != null && genderComboBox.getSelectionModel().getSelectedItem() != null)
                 form.put("gender", genderComboBox.getSelectionModel().getSelectedItem().getValue());
-            form.put("birthday", birthdayPick.getEditor().getText());
+            String birthdayTemp = birthdayPick.getEditor().getText();
+            if (!birthdayTemp.matches("^(?:(?:19|20)\\d{2})-(?:(?:0[1-9]|1[0-2]))-(?:(?:0[1-9]|[12]\\d|3[01]))$")){
+                MessageDialog.showDialog("日期不符合规范,请检查。");
+                return;
+            } else {
+                form.put("birthday", birthdayPick.getEditor().getText());
+            }
             form.put("email", emailField.getText());
             form.put("phone", phoneField.getText());
             form.put("address", addressField.getText());
@@ -319,14 +379,7 @@ public class teacherController extends ToolController {
             if (dataResponse != null && dataResponse.getCode() == 0){
                 MessageDialog.showDialog("更改成功！");
                 //刷新表格
-                DataRequest dataRequest1 = new DataRequest();
-                dataRequest1.add("page", 0);
-                dataRequest1.add("size", 50);
-                DataResponse dataResponse1 = HttpRequestUtil.request("/api/teacher/getTeacherList", dataRequest1);
-                if (dataResponse1 != null && dataResponse1.getCode() == 0){
-                    teacherList = (ArrayList<Map<String, String>>) dataResponse1.getData();
-                    setTableViewData();
-                }
+                refreshTeacherList();
             }
         }
     }
